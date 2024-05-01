947d991429ed8d1174a969988e080edace27c9ff
Piyush Gupta <piyushgupta1165@gmail.com>
2024-05-01 15:29:11 +0530

    initial commit

---
+++ server/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+
+const MONGO_URL = process.env.MONGO_URL;
+// const mongoUrl = MONGO_URL;
+const mongoUrl = "mongodb+srv://anshg1165:3uNT4bMdBTdERcrZ@root.1sofuak.mongodb.net/";
+
+mongoose.connect(mongoUrl, {})
+const db = mongoose.connection;
+
+db.on("connected", () => {
+    console.log("db connected");
+});
+
+db.on("disconnected", () => {
+    console.log("db disconnected");
+});
+
+db.on('error', (err) => {
+    console.log("error => ",err)
+});
+
+module.exports = db;
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const db = require("./db");
+
+const app = express();
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("Server is running on port 3000");
+});
