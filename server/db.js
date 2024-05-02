0f3c339edc71ef786834dcaa5c550fd7f78c6d67
Piyush Gupta <piyushgupta1165@gmail.com>
2024-05-02 12:48:26 +0530

    added post and user schema

--- server/db.js
+++ server/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-
 const MONGO_URL = process.env.MONGO_URL;
 // const mongoUrl = MONGO_URL;
 const mongoUrl = "mongodb+srv://anshg1165:3uNT4bMdBTdERcrZ@root.1sofuak.mongodb.net/";
---
+++ server/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    bio:{
+        type: String,
+        required: true
+    },
+
+    profilePic:{
+        type: String,
+        required: true
+    },
+    location:{
+        type: String,
+        required: true
+    },
+    password: {
+        required: true,
+        type: String,
+        select: true
+    },
+    createdAt:{
+
+    }
+},
+{
+    timestamps: true
+}
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = Person;
