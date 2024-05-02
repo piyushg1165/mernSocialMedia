b04459d2023bb7aa474acf91a435c52e6db9e25c
Piyush Gupta <piyushgupta1165@gmail.com>
2024-05-02 15:44:51 +0530

    added routes for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from'./pages/Home';
+import SignIn from'./pages/SignIn';
+import SignUp from'./pages/SignUp';
+import Profile from'./pages/Profile';
 
 function App() {
+  
 
   return (
     <>
-      <h1>hello</h1>
+    <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/signin" element={<SignIn />} />
+    <Route path="/signup" element={<SignUp />} />
+    <Route path="/profile" element={<Profile />} />
+  </Routes>
+</BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
